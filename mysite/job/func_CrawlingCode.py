ed84448c60a2bf32f84397a5a45812f19e227d39
ChungSeok-Lee <lcs625@naver.com>
2021-03-15 22:42:49 +0900

    lcs

--- mysite/job/func_CrawlingCode.py
+++ mysite/job/func_CrawlingCode.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 import os
 import pymysql
 
+
 #-- 크롬드라이버 설정 
 chrome_option = Options() #초기화
 chrome_option.add_argument("headless")
@@ -10,49 +11,50 @@ path = os.getcwd() # 설치한 Chromdriver 절대 경로설정
 driver  = webdriver.Chrome(path+'/chromedriver.exe', chrome_options= chrome_option) # driver 선언
 
 #-- 임시 페이지 연결
-driver.get('file:///C:/Users/pc1/Documents/%230.LCS/project_job/code.html') # 임시 페이지 절대 경로
+driver.get('file:///C:/Users/LCS/Documents/Project/99.Personal_JobAbility/code.html') # 임시 페이지 절대 경로
 
 #-- 저장할 DB 연결
-mysql = pymysql.connect(host='3.36.125.12', port=56754, user='lcs', password='lcs', db='tempdb', charset='utf8mb4', autocommit=True)
+mysql = pymysql.connect(host='3.36.125.12', port=58171, user='lcs', password='lcs', db='tempdb', charset='utf8mb4', autocommit=True)
 cursor = mysql.cursor(pymysql.cursors.DictCursor)
 query_1 = 'INSERT INTO Code_parent VALUES ("%s", "%s")'
+query_2 = 'INSERT INTO Code_details VALUES ("%s", "%s", "%s")'
 
-#-- 직군 분류 코드 가져오기
-# for i in range(11): # 대분류 
-#     try:
-#         xp_1 = '/html/body/div/div/div/div[%s]/a[1]'  # 대분류_코드 num xpath
-#         code_num = driver.find_element_by_xpath(xp_1 % str(i+1)).get_attribute('href')
-#         code_num = code_num.split('wdlist/')[-1]
-#         xp_2 = '/html/body/div/div/div/div[%s]/a[1]/h2' # 대분류_코드 name xpath
-#         code_name = driver.find_element_by_xpath(xp_2 % str(i+1)).text
-#         # print(query % (str(code_num), str(code_name)))
-#         cursor.execute(query_1 % (str(code_num), str(code_name)))
-#     except:
-#         print('pass')
-
+# 주요 직군 코드 
 for i in range(1, 12):
     xp_home = '/html/body/div/div/div/div[%s]/a[%s]'
-    
     for j in range(1, 10):
-        elements = driver.find_element_by_xpath(xp_home % (str(i), str(j)))
-        elements_split=elements.get_attribute('href').split('/wdlist')[-1]
-        num = elements_split.split('/')
-        if num == 2:
-            code_num = elements_split.split('/')[-1]
-            xp_name = xp_home % (str(i), str(j)) + '/h2'
-            code_name = driver.find_element_by_xpath(xp_name).text
-            print(code_num, code_name)
-        elif num ==3:
-            code_num = elements_split.split('/')[-2]
-            code_details_num = elements_split.split('/')[-1]
-            xp_name = xp_home % (str(i), str(j)) + '/h3'
-            code_name = driver.find_element_by_xpath(xp_name).text
-            print(code_num, code_details_num, code_name)
+        try:
+            elements = driver.find_element_by_xpath(xp_home % (str(i), str(j)))
+            elements_href = elements.get_attribute('href')
+            elements_split = elements_href.split('/wdlist')[-1]
+            num = len(elements_split.split('/'))
+            if num == 2:
+                code_num = elements_split.split('/')[-1]
+                xp_name = xp_home % (str(i), str(j)) + '/h2'
+                code_name = driver.find_element_by_xpath(xp_name).text
+                cursor.execute(query_1 % (code_num, code_name))
+            elif num ==3:
+                code_num = elements_split.split('/')[-2]
+                code_details_num = elements_split.split('/')[-1]
+                xp_name = xp_home % (str(i), str(j)) + '/h3'
+                code_name = driver.find_element_by_xpath(xp_name).text
+                cursor.execute(query_2 % (code_num, code_details_num, code_name))
+        except:
+            pass
+
+
+# 기타 직군 코드
+others_num = '999'
+cursor.execute('INSERT INTO Code_parent VALUES ("999", "기타")')
+for i in range(1, 10):
+    xp = '/html/body/div/div/div/div[12]/a[%s]'
+    elements = driver.find_element_by_xpath(xp % str(i))
+    elements_href= elements.get_attribute('href')
+    elements_split = elements_href.split('/wdlist')[-1]
+    code_details_num = elements_split.split('/')[-1]
+    code_name = driver.find_element_by_xpath(xp % str(i) + '/h2').text
+    cursor.execute(query_2 % (others_num, code_details_num, code_name))
+
 
 mysql.close()
 
-# # /html/body/div/div/div/div[4]/a[1]/h2
-# # /html/body/div/div/div/div[4]/a[8]/h3
-# /html/body/div/div/div/div[12]/a[5]/h2
-# /html/body/div/div/div/div[12]/a[9]/h2
-# /html/body/div/div/div/div[11]/a[8]/h3
